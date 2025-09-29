2a536f8e293e19f8bdfaf11d8fa9153ab7c87b68
Diogo Luís <7diogo.luis@gmail.com>
2025-09-29 21:20:12 +0100

    Add files via upload

--- ZIRCON.py
+++ ZIRCON.py
@@ -6,7 +6,7 @@ file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
 from subsystems.controller import controller
 
-persist = {'sw_version': 'v0.23.0',
+persist = {'sw_version': 'v0.23.1',
            'loaded_layout': False,
            'processed_layout': False,
            'usr_request': None,
